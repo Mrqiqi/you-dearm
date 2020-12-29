304821b7905e23f3ac2c028f55c69c23a4365ca7
Mr.qiqi <1464754606@qq.com>
2020-12-29 09:19:24 +0800

    'init'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip= false
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el:"#app",
+  render:h =>h(App)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
